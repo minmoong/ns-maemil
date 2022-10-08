8e54b6f54dc82be7417a2dc1ee8d8d6f51f48e09
Minkyu Kim <kim774634@gmail.com>
2022-10-08 04:15:28 +0000

    update

--- src/routes/api/getTimetable/+server.ts
+++ src/routes/api/getTimetable/+server.ts
@@ -6,10 +6,12 @@ const Timetable = require('comcigan-parser')
 const timetable = new Timetable()
 
 export const GET: RequestHandler = async () => {
-    console.log('이거 보이니?이거 보이니?이거 보이니?이거 보이니?이거 보이니?이거 보이니?이거 보이니?이거 보이니?이거 보이니?이거 보이니?')
-    console.log(import.meta.url)
-    
-    await timetable.init()
-    await timetable.setSchool(65332)
-    return json(await timetable.getTimetable())
+    // console.log('이거 보이니?이거 보이니?이거 보이니?이거 보이니?이거 보이니?이거 보이니?이거 보이니?이거 보이니?이거 보이니?이거 보이니?')
+    // console.log(import.meta.url)
+
+    // await timetable.init()
+    // await timetable.setSchool(65332)
+    // return json(await timetable.getTimetable())
+    console.log('hi')
+    return json(1)
 }
