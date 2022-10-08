36f0e96e582099110b18c3d8a57052b81f170614
Minkyu Kim <kim774634@gmail.com>
2022-10-08 03:18:54 +0000

    update

--- src/routes/api/getTimetable/+server.ts
+++ src/routes/api/getTimetable/+server.ts
@@ -2,11 +2,11 @@ import { json, type RequestHandler } from '@sveltejs/kit'
 import { createRequire } from 'module'
 
 const require = createRequire(import.meta.url)
-console.log(import.meta.url)
 const Timetable = require('comcigan-parser')
 const timetable = new Timetable()
 
 export const GET: RequestHandler = async () => {
+  console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: ", import.meta.url)
   await timetable.init()
   await timetable.setSchool(65332)
   return json(await timetable.getTimetable())
